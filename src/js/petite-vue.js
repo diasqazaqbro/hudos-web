f9521faadc8b8e8e9326fd9c4406d9396b380826
diasqazaqbro <diasqazaqbro@mail.ru>
2023-11-23 20:51:56 +0600

    maybe realese

--- src/js/petite-vue.js
+++ src/js/petite-vue.js
@@ -20,10 +20,10 @@ const store = reactive({
       threeSupTitle: 'loading...',
     },
     objects: {
-      mainNumber: 'loading...',
-      numberOne: 'loading...',
-      numberTwo: 'loading...',
-      numberThree: 'loading...',
+      mainNumber: 0,
+      numberOne: 0,
+      numberTwo: 0,
+      numberThree: 0,
     },
     services: {
       oneTitle: 'loading...',
@@ -39,7 +39,21 @@ const store = reactive({
       fourSupTitle: 'loading...',
       fourDesc: 'loading...',
     },
-    settings: { number: 'loading...', },
+    price: {
+      oneTitle: 'loading...',
+      oneSupTitle: 'loading...',
+      oneDesc: 'loading...',
+      twoTitle: 'loading...',
+      twoSupTitle: 'loading...',
+      twoDesc: 'loading...',
+      threeTitle: 'loading...',
+      threeSupTitle: 'loading...',
+      threeDesc: 'loading...',
+      fourTitle: 'loading...',
+      fourSupTitle: 'loading...',
+      fourDesc: 'loading...',
+    },
+    settings: { number: 'loading...' },
     stages: {
       oneTitle: 'loading...',
       oneDesc: 'loading...',
@@ -58,7 +72,20 @@ const store = reactive({
       eightTitle: 'loading...',
       eightDesc: 'loading...',
     },
-    welcome: { welcomeTitle: 'loading...', welcomeSupTitle: 'loading...', welcomeDesc: 'loading...'},
+    footer: {
+      address: 'loading...',
+      workTime: 'loading...',
+      holiday: 'loading...',
+      clientNumber: 'loading...',
+      clientEmail: 'loading...',
+      partnerNumber: 'loading...',
+      partnerEmail: 'loading...',
+    },
+    welcome: {
+      welcomeTitle: 'loading...',
+      welcomeSupTitle: 'loading...',
+      welcomeDesc: 'loading...',
+    },
     images: {
       i1: '',
       i2: '',
@@ -89,8 +116,7 @@ const store = reactive({
       aTitleOne: 'loading...',
       aTitleTwo: 'loading...',
       aTitleThree: 'loading...',
-    }
-  
+    },
   },
   isModalOpen: false,
   anotherProject: '',
@@ -157,176 +183,239 @@ const store = reactive({
   },
   fetchData() {
     axios.get('https://hudos-admin.vercel.app/api/faq').then((resp) => {
-      const faq = store.fetchResult.faq;
-      faq.oneTitle = resp.data[0].oneTitle;
-      faq.oneSupTitle = resp.data[0].oneSupTitle;
-      faq.twoTitle = resp.data[0].twoTitle;
-      faq.twoSupTitle = resp.data[0].twoSupTitle;
-      faq.threeTitle = resp.data[0].threeTitle;
-      faq.threeSupTitle = resp.data[0].threeSupTitle;
-    });
-    
+      const faq = store.fetchResult.faq
+      faq.oneTitle = resp.data[0].oneTitle
+      faq.oneSupTitle = resp.data[0].oneSupTitle
+      faq.twoTitle = resp.data[0].twoTitle
+      faq.twoSupTitle = resp.data[0].twoSupTitle
+      faq.threeTitle = resp.data[0].threeTitle
+      faq.threeSupTitle = resp.data[0].threeSupTitle
+    })
+
     axios.get('https://hudos-admin.vercel.app/api/objects').then((resp) => {
       const objects = store.fetchResult.objects;
-      objects.mainNumber = resp.data[0].mainNumber;
-      objects.numberOne = resp.data[0].numberOne;
-      objects.numberTwo = resp.data[0].numberTwo;
-      objects.numberThree = resp.data[0].numberThree;
+      const responseData = resp.data[0];
+      objects.mainNumber = parseInt(responseData.mainNumber, 10);
+      objects.numberOne = parseInt(responseData.numberOne, 10);
+      objects.numberTwo = parseInt(responseData.numberTwo, 10);
+      objects.numberThree = parseInt(responseData.numberThree, 10);
     });
-    
     axios.get('https://hudos-admin.vercel.app/api/services').then((resp) => {
-      const services = store.fetchResult.services;
-      services.oneTitle = resp.data[0].oneTitle;
-      services.oneSupTitle = resp.data[0].oneSupTitle;
-      services.oneDesc = resp.data[0].oneDesc;
-      services.twoTitle = resp.data[0].twoTitle;
-      services.twoSupTitle = resp.data[0].twoSupTitle;
-      services.twoDesc = resp.data[0].twoDesc;
-      services.threeTitle = resp.data[0].threeTitle;
-      services.threeSupTitle = resp.data[0].threeSupTitle;
-      services.threeDesc = resp.data[0].threeDesc;
-      services.fourTitle = resp.data[0].fourTitle;
-      services.fourSupTitle = resp.data[0].fourSupTitle;
-      services.fourDesc = resp.data[0].fourDesc;
-    });
-    
+      const services = store.fetchResult.services
+      services.oneTitle = resp.data[0].oneTitle
+      services.oneSupTitle = resp.data[0].oneSupTitle
+      services.oneDesc = resp.data[0].oneDesc
+      services.twoTitle = resp.data[0].twoTitle
+      services.twoSupTitle = resp.data[0].twoSupTitle
+      services.twoDesc = resp.data[0].twoDesc
+      services.threeTitle = resp.data[0].threeTitle
+      services.threeSupTitle = resp.data[0].threeSupTitle
+      services.threeDesc = resp.data[0].threeDesc
+      services.fourTitle = resp.data[0].fourTitle
+      services.fourSupTitle = resp.data[0].fourSupTitle
+      services.fourDesc = resp.data[0].fourDesc
+    })
+    axios.get('https://hudos-admin.vercel.app/api/price').then((resp) => {
+      const price = store.fetchResult.price
+      price.oneTitle = resp.data[0].oneTitle
+      price.oneSupTitle = resp.data[0].oneSupTitle
+      price.oneDesc = resp.data[0].oneDesc
+      price.twoTitle = resp.data[0].twoTitle
+      price.twoSupTitle = resp.data[0].twoSupTitle
+      price.twoDesc = resp.data[0].twoDesc
+      price.threeTitle = resp.data[0].threeTitle
+      price.threeSupTitle = resp.data[0].threeSupTitle
+      price.threeDesc = resp.data[0].threeDesc
+      price.fourTitle = resp.data[0].fourTitle
+      price.fourSupTitle = resp.data[0].fourSupTitle
+      price.fourDesc = resp.data[0].fourDesc
+    })
+
     axios.get('https://hudos-admin.vercel.app/api/settings').then((resp) => {
-      const settings = store.fetchResult.settings;
-      settings.number = resp.data[0].number;
-    });
-    
+      const settings = store.fetchResult.settings
+      settings.number = resp.data[0].number
+    })
+
     axios.get('https://hudos-admin.vercel.app/api/stages').then((resp) => {
-      const stages = store.fetchResult.stages;
-      stages.oneTitle = resp.data[0].oneTitle;
-      stages.oneDesc = resp.data[0].oneDesc;
-      stages.twoTitle = resp.data[0].twoTitle;
-      stages.twoDesc = resp.data[0].twoDesc;
-      stages.threeTitle = resp.data[0].threeTitle;
-      stages.threeDesc = resp.data[0].threeDesc;
-      stages.fourTitle = resp.data[0].fourTitle;
-      stages.fourDesc = resp.data[0].fourDesc;
-      stages.fiveTitle = resp.data[0].fiveTitle;
-      stages.fiveDesc = resp.data[0].fiveDesc;
-      stages.sixTitle = resp.data[0].sixTitle;
-      stages.sixDesc = resp.data[0].sixDesc;
-      stages.sevenTitle = resp.data[0].sevenTitle;
-      stages.sevenDesc = resp.data[0].sevenDesc;
-      stages.eightTitle = resp.data[0].eightTitle;
-      stages.eightDesc = resp.data[0].eightDesc;
-    });
+      const stages = store.fetchResult.stages
+      stages.oneTitle = resp.data[0].oneTitle
+      stages.oneDesc = resp.data[0].oneDesc
+      stages.twoTitle = resp.data[0].twoTitle
+      stages.twoDesc = resp.data[0].twoDesc
+      stages.threeTitle = resp.data[0].threeTitle
+      stages.threeDesc = resp.data[0].threeDesc
+      stages.fourTitle = resp.data[0].fourTitle
+      stages.fourDesc = resp.data[0].fourDesc
+      stages.fiveTitle = resp.data[0].fiveTitle
+      stages.fiveDesc = resp.data[0].fiveDesc
+      stages.sixTitle = resp.data[0].sixTitle
+      stages.sixDesc = resp.data[0].sixDesc
+      stages.sevenTitle = resp.data[0].sevenTitle
+      stages.sevenDesc = resp.data[0].sevenDesc
+      stages.eightTitle = resp.data[0].eightTitle
+      stages.eightDesc = resp.data[0].eightDesc
+    })
     axios.get('https://hudos-admin.vercel.app/api/projects').then((resp) => {
-      const projects = store.fetchResult.projects;
-      projects.designTitleOne = resp.data[0].designTitleOne;
-      projects.designTitleTwo = resp.data[0].designTitleTwo;
-      projects.designTitleThree = resp.data[0].designTitleThree;
-      projects.designTitleFour = resp.data[0].designTitleFour;
-      projects.designTitleFive = resp.data[0].designTitleFive;
-      projects.aTitleOne = resp.data[0].aTitleOne;
-      projects.aTitleTwo = resp.data[0].aTitleTwo;
-      projects.aTitleThree = resp.data[0].aTitleThree;
-    });
-    
+      const projects = store.fetchResult.projects
+      projects.designTitleOne = resp.data[0].designTitleOne
+      projects.designTitleTwo = resp.data[0].designTitleTwo
+      projects.designTitleThree = resp.data[0].designTitleThree
+      projects.designTitleFour = resp.data[0].designTitleFour
+      projects.designTitleFive = resp.data[0].designTitleFive
+      projects.aTitleOne = resp.data[0].aTitleOne
+      projects.aTitleTwo = resp.data[0].aTitleTwo
+      projects.aTitleThree = resp.data[0].aTitleThree
+    })
+    axios.get('https://hudos-admin.vercel.app/api/footer').then((resp) => {
+      const footer = store.fetchResult.footer
+      footer.address = resp.data[0].address
+      footer.workTime = resp.data[0].workTime
+      footer.holiday = resp.data[0].holiday
+      footer.clientNumber = resp.data[0].clientNumber
+      footer.clientEmail = resp.data[0].clientEmail
+      footer.partnerNumber = resp.data[0].partnerNumber
+      footer.partnerEmail = resp.data[0].partnerEmail
+    })
+
     axios.get('https://hudos-admin.vercel.app/api/welcome').then((resp) => {
-      const welcome = store.fetchResult.welcome;
-      welcome.welcomeTitle = resp.data[0].welcomeTitle;
-      welcome.welcomeSupTitle = resp.data[0].welcomeSupTitle;
-      welcome.welcomeDesc = resp.data[0].welcomeDesc;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=1').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i1 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=2').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i2 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=3').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i3 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=4').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i4 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=5').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i5 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=6').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i6 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=7').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i7 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=8').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i8 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=9').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i9 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=10').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i10 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=11').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i11 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=12').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i12 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=13').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i13 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=14').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i14 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=15').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i15 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=16').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i16 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=17').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i17 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=18').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i18 = path;
-    });
-    axios.get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=19').then((resp) => {
-      const images = store.fetchResult.images;
-      const path = resp.data.results.path
-      images.i19 = path;
-    });
+      const welcome = store.fetchResult.welcome
+      welcome.welcomeTitle = resp.data[0].welcomeTitle
+      welcome.welcomeSupTitle = resp.data[0].welcomeSupTitle
+      welcome.welcomeDesc = resp.data[0].welcomeDesc
+    })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=1')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i1 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=2')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i2 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=3')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i3 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=4')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i4 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=5')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i5 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=6')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i6 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=7')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i7 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=8')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i8 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=9')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i9 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=10')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i10 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=11')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i11 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=12')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i12 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=13')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i13 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=14')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i14 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=15')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i15 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=16')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i16 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=17')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i17 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=18')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i18 = path
+      })
+    axios
+      .get('https://timkaqwerty.pythonanywhere.com/hds/img/?id=19')
+      .then((resp) => {
+        const images = store.fetchResult.images
+        const path = resp.data.results.path
+        images.i19 = path
+      })
   },
   checkErrors(step) {
     this.clearError()
